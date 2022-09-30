1fe173390c3766aa91c5198ff12f5946e6691367
Claverias123 <al_amartinezc@iessierradelasvillas.org>
2022-09-30 11:13:35 +0200

    Subir ejercicio 2

---
+++ EJERCICIO 2.py
@@ -0,0 +1,7 @@
+#Calcular el perímetro y área de un rectángulo dada su base y su altura.
+base=0
+lado=0
+
+base= (int) (input  ("Dime un número para la base "))
+lado= (int) (input  ("Dime un número para el lado "))
+print  ("El perimetro es", (base*lado))
