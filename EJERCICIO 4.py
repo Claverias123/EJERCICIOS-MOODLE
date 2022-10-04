d8e66672145ef899f067633fc84b8b6c0add4955
Claverias123 <al_amartinezc@iessierradelasvillas.org>
2022-10-04 14:40:44 +0200

    4,8,6

--- EJERCICIO 4.py
+++ EJERCICIO 4.py
@@ -4,8 +4,6 @@
 
 numero_1 = (int) (input ("Dime un número: \n"))
 numero_2 = (int) (input ("Dime otro münero:\n"))
-
-
 print("La suma de esos números es:" , numero_1+numero_2)
 print("la resta de esos números es : ", numero_1-numero_2)
 print("la multiplicación de esos números es:" ,numero_1*numero_2)
